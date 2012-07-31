a08582bdbcd80ce88ceffdff8c7a19a0de0630df
Andrés Torres <elfotografo007@gmail.com>
2012-07-31 16:54:58 -0500

    Se crea rails vacío

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ServidorActi::Application.config.secret_token = '29711c9b0670862138fd1edf7aac1d296fb85e1a3b98ce62d38793bd788bb252de258fd58efe9b9f7a898ac34a2d723e75584ad74a6a27934d6fe31f779e264c'
